fab0680aae854df17b9dc3bcafbd4245edf45e22
Roman <katyrinroman@mail.ru>
2021-06-12 13:17:46 +0700

    added cache ability RetrofitGithubRepositoriesRepo and RetrofitGithubUsersRepo

--- app/src/main/java/com/katyrin/githubusers/repository/RetrofitGithubRepositoriesRepo.kt
+++ app/src/main/java/com/katyrin/githubusers/repository/RetrofitGithubRepositoriesRepo.kt
@@ -3,16 +3,48 @@ package com.katyrin.githubusers.repository
 import com.katyrin.githubusers.api.IDataSource
 import com.katyrin.githubusers.data.GitHubRepository
 import com.katyrin.githubusers.data.GithubUser
-import com.katyrin.githubusers.utils.ERROR_REPO_LIST
+import com.katyrin.githubusers.data.room.Database
+import com.katyrin.githubusers.data.room.RoomGithubRepository
+import com.katyrin.githubusers.network.INetworkStatus
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class RetrofitGithubRepositoriesRepo(val api: IDataSource) : IGithubRepositoriesRepo {
-    override fun getRepositories(user: GithubUser): Single<List<GitHubRepository>> =
-        user.reposUrl?.let { url ->
-            api.getRepositories(url)
-                .subscribeOn(Schedulers.io())
-        } ?: Single
-            .error<List<GitHubRepository>>(RuntimeException(ERROR_REPO_LIST))
-            .subscribeOn(Schedulers.io())
+class RetrofitGithubRepositoriesRepo(
+    val api: IDataSource,
+    val networkStatus: INetworkStatus,
+    val db: Database
+) : IGithubRepositoriesRepo {
+    override fun getRepositories(user: GithubUser) =
+        networkStatus.isOnlineSingle().flatMap { isOnline ->
+            if (isOnline) {
+                user.reposUrl?.let { url ->
+                    api.getRepositories(url)
+                        .flatMap { repositories ->
+                            Single.fromCallable {
+                                val roomUser = user.login?.let { db.userDao.findByLogin(it) }
+                                    ?: throw RuntimeException("No such user in cache")
+                                val roomRepos = repositories.map {
+                                    RoomGithubRepository(
+                                        it.id ?: "",
+                                        it.name ?: "",
+                                        it.forksCount ?: 0,
+                                        roomUser.id
+                                    )
+                                }
+                                db.repositoryDao.insert(roomRepos)
+                                repositories
+                            }
+                        }
+                } ?: Single.error<List<GitHubRepository>>(RuntimeException("User has no repos url"))
+                    .subscribeOn(Schedulers.io())
+            } else {
+                Single.fromCallable {
+                    val roomUser = user.login?.let { db.userDao.findByLogin(it) }
+                        ?: throw RuntimeException("No such user in cache")
+                    db.repositoryDao.findForUser(roomUser.id)
+                        .map { GitHubRepository(it.id, it.name, it.forksCount) }
+                }
+
+            }
+        }.subscribeOn(Schedulers.io())
 }
--- app/src/main/java/com/katyrin/githubusers/repository/RetrofitGithubUsersRepo.kt
+++ app/src/main/java/com/katyrin/githubusers/repository/RetrofitGithubUsersRepo.kt
@@ -2,14 +2,48 @@ package com.katyrin.githubusers.repository
 
 import com.katyrin.githubusers.api.IDataSource
 import com.katyrin.githubusers.data.GithubUser
-import com.katyrin.githubusers.utils.ERROR_NOT_FOUND_USERS
+import com.katyrin.githubusers.data.room.Database
+import com.katyrin.githubusers.data.room.RoomGithubUser
+import com.katyrin.githubusers.network.INetworkStatus
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class RetrofitGithubUsersRepo(val api: IDataSource) : IGithubUsersRepo {
+class RetrofitGithubUsersRepo(
+    val api: IDataSource,
+    val networkStatus: INetworkStatus,
+    val db: Database
+) : IGithubUsersRepo {
     override fun getUsers(): Single<List<GithubUser>> =
-        api.getUsers().subscribeOn(Schedulers.io()) ?: Single.error<List<GithubUser>>(
-            RuntimeException(ERROR_NOT_FOUND_USERS)
-        )
+        networkStatus.isOnlineSingle()
+            .flatMap { isOnline ->
+                if (isOnline) {
+                    api.getUsers()
+                        .flatMap { users ->
+                            Single.fromCallable {
+                                val roomUsers = users.map { user ->
+                                    RoomGithubUser(
+                                        user.id ?: "",
+                                        user.login ?: "",
+                                        user.avatarUrl ?: "",
+                                        user.reposUrl ?: ""
+                                    )
+                                }
+                                db.userDao.insert(roomUsers)
+                                users
+                            }
+                        }
+                } else {
+                    Single.fromCallable {
+                        db.userDao.getAll().map { roomUser ->
+                            GithubUser(
+                                roomUser.id,
+                                roomUser.login,
+                                roomUser.avatarUrl,
+                                roomUser.reposUrl
+                            )
+                        }
+                    }
+                }
+            }
             .subscribeOn(Schedulers.io())
 }
